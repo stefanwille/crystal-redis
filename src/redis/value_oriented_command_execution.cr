3772d50f898930d5c650a019ec0c1e31d15680dc
Stefan Wille <github@stefanwille.com>
2015-05-13 22:03:34 +0200

    Correct namespacing

--- src/redis/value_oriented_command_execution.cr
+++ src/redis/value_oriented_command_execution.cr
@@ -1,63 +1,65 @@
 # Command execution methods that return real values, not futures.
 #
-module Redis::ValueOrientedCommandExecution
-  # Executes a Redis command and casts it to the correct type.
-  # This is an internal method.
-  def integer_command(request : Request)
-    command(request) as Int64
-  end
+class Redis
+  module ValueOrientedCommandExecution
+    # Executes a Redis command and casts it to the correct type.
+    # This is an internal method.
+    def integer_command(request : Request)
+      command(request) as Int64
+    end
 
-  # Executes a Redis command and casts it to the correct type.
-  # This is an internal method.
-  def integer_or_nil_command(request : Request)
-    command(request) as Int64?
-  end
+    # Executes a Redis command and casts it to the correct type.
+    # This is an internal method.
+    def integer_or_nil_command(request : Request)
+      command(request) as Int64?
+    end
 
-  # Executes a Redis command and casts it to the correct type.
-  # This is an internal method.
-  def integer_array_command(request : Request)
-    command(request) as Array(RedisValue)
-  end
+    # Executes a Redis command and casts it to the correct type.
+    # This is an internal method.
+    def integer_array_command(request : Request)
+      command(request) as Array(RedisValue)
+    end
 
-  # Executes a Redis command and casts it to the correct type.
-  # This is an internal method.
-  def string_command(request : Request)
-    command(request) as String
-  end
+    # Executes a Redis command and casts it to the correct type.
+    # This is an internal method.
+    def string_command(request : Request)
+      command(request) as String
+    end
 
-  # Executes a Redis command and casts the response to the correct type.
-  # This is an internal method.
-  def string_or_nil_command(request : Request)
-    command(request) as String?
-  end
+    # Executes a Redis command and casts the response to the correct type.
+    # This is an internal method.
+    def string_or_nil_command(request : Request)
+      command(request) as String?
+    end
 
-  # Executes a Redis command and casts the response to the correct type.
-  # This is an internal method.
-  def string_array_command(request : Request)
-    command(request) as Array(RedisValue)
-  end
+    # Executes a Redis command and casts the response to the correct type.
+    # This is an internal method.
+    def string_array_command(request : Request)
+      command(request) as Array(RedisValue)
+    end
 
-  # Executes a Redis command and casts the response to the correct type.
-  # This is an internal method.
-  def string_array_or_integer_command(request : Request)
-    command(request) as Array(RedisValue) | Int64
-  end
+    # Executes a Redis command and casts the response to the correct type.
+    # This is an internal method.
+    def string_array_or_integer_command(request : Request)
+      command(request) as Array(RedisValue) | Int64
+    end
 
-  # Executes a Redis command and casts the response to the correct type.
-  # This is an internal method.
-  def string_array_or_string_command(request : Request)
-    command(request) as Array(RedisValue) | String
-  end
+    # Executes a Redis command and casts the response to the correct type.
+    # This is an internal method.
+    def string_array_or_string_command(request : Request)
+      command(request) as Array(RedisValue) | String
+    end
 
-  # Executes a Redis command and casts the response to the correct type.
-  # This is an internal method.
-  def array_or_nil_command(request : Request)
-    command(request) as Array(RedisValue)?
-  end
+    # Executes a Redis command and casts the response to the correct type.
+    # This is an internal method.
+    def array_or_nil_command(request : Request)
+      command(request) as Array(RedisValue)?
+    end
 
-  # Executes a Redis command that has no relevant response.
-  # This is an internal method.
-  def void_command(request : Request)
-    command(request)
+    # Executes a Redis command that has no relevant response.
+    # This is an internal method.
+    def void_command(request : Request)
+      command(request)
+    end
   end
 end
