8008932503665a4f38bfcd2f536642966e95cd25
Stefan Wille <github@stefanwille.com>
2015-06-05 18:39:42 +0200

    Workaround for issue #2: Pipeline hangs on Ubuntu 12.04 / Redis 2.8
    On Ubuntu 12.04 / Redis 2.8 / Crystal 0.7.2, a pipeline
    with 1 million requests may hang.
    
    With C/Hiredis, the issue does not occur. This seems to be caused
    by either Crystal-Redis or Crystal.
    
    I saw the issue on Ubuntu 14.04, but only once.
    
    My workaround is to fetch all outstanding responses after every
    100,000 requests. This turns the pipeline effectively into
    several smaller pipelines.

--- src/redis/strategy/pipeline.cr
+++ src/redis/strategy/pipeline.cr
@@ -4,27 +4,51 @@
 #
 # Used in Redis#pipelined.
 class Redis::Strategy::Pipeline < Redis::Strategy::Base
+  MAX_OUTSTANDING_RESPONSES = 10_000
+
   def initialize(@connection)
     @futures = [] of Redis::Future
+    @results = [] of RedisValue
+    @outstanding_responses = 0
   end
 
   def command(request : Request) : Redis::Future
     @connection.queue(request)
     future = Future.new
     @futures << future
+    # Don't send too many requests without reading their responses.
+    # Otherwise we can trigger issue #2 (https://github.com/stefanwille/crystal-redis/issues/2)
+    @outstanding_responses += 1
+    limit_outstanding_responses
     future
   end
 
   def commit
+    receive_all_outstanding_responses
+    give_each_future_its_value
+    @results
+  end
+
+  private def limit_outstanding_responses
+    if @outstanding_responses > MAX_OUTSTANDING_RESPONSES
+      receive_all_outstanding_responses
+    end
+  end
+
+  private def receive_all_outstanding_responses
     flush
 
-    results = [] of RedisValue
-    @futures.each_with_index do |future, i|
+    @outstanding_responses.times do |i|
       response = @connection.receive
-      results << response
-      future.value = response
+      @results << response
     end
-    results
+    @outstanding_responses = 0
+  end
+
+  private def give_each_future_its_value
+    @futures.each_with_index do |future, i|
+      future.value = @results[i]
+    end
   end
 
   private def flush
