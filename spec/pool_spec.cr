31f040a182887d62d7a1e72821f14944c67d7434
Stefan Wille <github@stefanwille.com>
2018-06-18 10:00:28 +0200

    Remove noise from the spec

--- spec/pool_spec.cr
+++ spec/pool_spec.cr
@@ -15,12 +15,6 @@ describe Redis::Pool do
         redis_pool = Redis::Pool.new
         redis_pool.close
       end
-
-      # it "tolerates a duplicate call" do
-      #   redis_pool = Redis::Pool.new
-      #   redis_pool.close
-      #   redis_pool.close
-      # end
     end
   end
 
@@ -28,7 +22,7 @@ describe Redis::Pool do
     it "provides a Redis instance to the current coroutine until an explicit call to #checkin" do
       redis_pool = Redis::Pool.new
       redis = redis_pool.checkout
-      redis.set("mike", "rules").should eq("OK")
+      redis.set("mike", "rules")
       redis.get("mike").should eq("rules")
       redis_pool.checkin
     end
@@ -38,7 +32,7 @@ describe Redis::Pool do
     it "executes the given block with a Redis instance from the pool" do
       redis_pool = Redis::Pool.new
       redis_pool.connection do |redis|
-        redis.set("mike", "rules").should eq("OK")
+        redis.set("mike", "rules")
         redis.get("mike").should eq("rules")
       end
     end
