01035a44da70227a7158810b5d44faf6b7734c73
Stefan Wille <github@stefanwille.com>
2018-06-22 09:40:36 +0200

    Remove class Redis::Client

--- src/redis/client.cr
+++
@@ -1,7 +0,0 @@
-class Redis::Client
-  getter connection : Redis::Connection
-  property strategy : Redis::Strategy::Base
-
-  def initialize(@connection : Redis::Connection, @strategy : Redis::Strategy::Base)
-  end
-end
