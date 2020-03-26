c46eb9fec07000332ff4ade598edc3fa4b06561e
Brian J. Cardiff <bcardiff@gmail.com>
2020-03-26 20:35:36 -0300

    Use TimeoutError and avoid Errno on Crystal 0.34

--- src/redis/socket_wrapper.cr
+++ src/redis/socket_wrapper.cr
@@ -6,29 +6,57 @@ struct Redis::SocketWrapper
     @connected = true
   end
 
-  def self.new
-    self.new(yield)
-  rescue ex : IO::Timeout | Errno | Socket::Error | OpenSSL::Error
-    raise Redis::CannotConnectError.new("#{ex.class}: #{ex.message}")
-  end
+  {% if compare_versions(Crystal::VERSION, "0.34.0-0") > 0 %}
+    def self.new
+      self.new(yield)
+    rescue ex : IO::TimeoutError | Socket::Error | OpenSSL::Error
+      raise Redis::CannotConnectError.new("#{ex.class}: #{ex.message}")
+    end
+  {% else %}
+    def self.new
+      self.new(yield)
+    rescue ex : IO::Timeout | Errno | Socket::Error | OpenSSL::Error
+      raise Redis::CannotConnectError.new("#{ex.class}: #{ex.message}")
+    end
+  {% end %}
 
   macro method_missing(call)
     catch_errors { @socket.{{call}} }
   end
 
-  private def catch_errors
-    yield
-  rescue ex : Errno | IO::Error | OpenSSL::Error
-    raise Redis::ConnectionLostError.new("#{ex.class}: #{ex.message}")
-  rescue ex : IO::Timeout
-    raise Redis::CommandTimeoutError.new("Command timed out")
-  end
-
-  def close
-    if @connected
-      @connected = false
-      @socket.close
+  {% if compare_versions(Crystal::VERSION, "0.34.0-0") > 0 %}
+    private def catch_errors
+      yield
+    rescue ex : IO::Error | OpenSSL::Error
+      raise Redis::ConnectionLostError.new("#{ex.class}: #{ex.message}")
+    rescue ex : IO::TimeoutError
+      raise Redis::CommandTimeoutError.new("Command timed out")
     end
-  rescue Errno
-  end
+  {% else %}
+    private def catch_errors
+      yield
+    rescue ex : Errno | IO::Error | OpenSSL::Error
+      raise Redis::ConnectionLostError.new("#{ex.class}: #{ex.message}")
+    rescue ex : IO::Timeout
+      raise Redis::CommandTimeoutError.new("Command timed out")
+    end
+  {% end %}
+
+  {% if compare_versions(Crystal::VERSION, "0.34.0-0") > 0 %}
+    def close
+      if @connected
+        @connected = false
+        @socket.close
+      end
+    rescue Socket::Error
+    end
+  {% else %}
+    def close
+      if @connected
+        @connected = false
+        @socket.close
+      end
+    rescue Errno
+    end
+  {% end %}
 end
